b41d6414434c01d9e4e8ada0063d29ac67644c93
officialtalha <31705132+officialtalha>
2023-03-25 21:31:20 +0530

    successfully write response

--- testRequest.js
+++ testRequest.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Talha is online');
+    console.log(req.url);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<html>
+    <header>
+    <title>response done!</title>
+    </header>
+    </body>
+    <h1> This is a response from the Node js server again </h1>
+    </body>
+    </html>`);
+    res.end();
 });
 
 server.listen(4000);
