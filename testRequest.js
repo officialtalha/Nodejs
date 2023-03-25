5e38455abef61f9e9283251d33b0a902737425ca
officialtalha <31705132+officialtalha>
2023-03-25 22:19:39 +0530

    response done to multiple pages using req.url

--- testRequest.js
+++ testRequest.js
@@ -2,16 +2,58 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.setHeader('Content-Type', 'text/html');
-    res.write(`<html>
-    <header>
-    <title>response done!</title>
-    </header>
-    </body>
-    <h1> This is a response from the Node js server again </h1>
-    </body>
-    </html>`);
-    res.end();
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html>
+        <header>
+        <title>response done!</title>
+        </header>
+        </body>
+        <h1> This is a response from the Node js server again </h1>
+        </body>
+        </html>`);
+        res.end();
+    }
+
+    if (url === '/home') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html>
+        <header>
+        <title>response done!</title>
+        </header>
+        </body>
+        <h1> Welcome Home </h1>
+        </body>
+        </html>`);
+        res.end();
+    }
+    if (url === '/about') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html>
+        <header>
+        <title>response done!</title>
+        </header>
+        </body>
+        <h1> Welcome To About Us Page </h1>
+        </body>
+        </html>`);
+        res.end();
+    }
+
+    if (url === '/node') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html>
+        <header>
+        <title>response done!</title>
+        </header>
+        </body>
+        <h1> Welcome To my Node JS Project </h1>
+        </body>
+        </html>`);
+        res.end();
+    }
+    
 });
 
 server.listen(4000);
