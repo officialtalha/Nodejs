327d4c8affdc836a5cdcb85407c3b3f0d26ea817
officialtalha <31705132+officialtalha>
2023-03-26 21:09:57 +0530

    Express Folder Created

--- testRequest.js
+++ testRequest.js
@@ -3,19 +3,19 @@ const express = require('express');
 const app = express();
 
 app.use('/sale',(req, res, next) => {
-    console.log('first middleware');
+    console.log('first-middleware');
     res.send('<h1>Sale page!</h1>');
     next();
 });
 
 
 app.use('/product',(req, res, next) => {
-    console.log('second middleware');
+    console.log('second-middleware');
     res.send('<h1>Product page!</h1>');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('third middleware');
+    console.log('third-middleware');
     res.send('<h1>Home page!</h1>');
     // next();
 });
