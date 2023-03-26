9b3d98b50351365fefb245bea843aac4920222e6
officialtalha <31705132+officialtalha>
2023-03-26 21:07:10 +0530

    Express folder created

--- testRequest.js
+++ testRequest.js
@@ -1,18 +1,27 @@
-const http = require('http');
-// const routes = require('./routes');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the first middleware.');
+app.use('/sale',(req, res, next) => {
+    console.log('first middleware');
+    res.send('<h1>Sale page!</h1>');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('In the second middleware.');
-    res.send({key1: 'value'});
-});
-const server = http.createServer(app);
 
-server.listen(4000);
+app.use('/product',(req, res, next) => {
+    console.log('second middleware');
+    res.send('<h1>Product page!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('third middleware');
+    res.send('<h1>Home page!</h1>');
+    // next();
+});
+
+
+
+app.listen(4000, () => {
+    console.log('server listening on port 4000');
+});
