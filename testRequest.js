d97eee944dace74cfcad7e65ac65e69471679940
officialtalha <31705132+officialtalha>
2023-03-27 16:01:01 +0530

    routes and filters

--- testRequest.js
+++ testRequest.js
@@ -1,29 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send(`<form action="/product" method="POST">
-            <input type="text" name="product">
-            <input type="text" name="quantity">
-            <button type="submit"> Add Product </button>
-            </form>`);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
 });
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send(`<h1>Home Page Express JS</h1>`);
-});
-
-
-app.listen(4000, () => {
-    console.log('server listening on port 4000');
-});
+app.listen(3000);
